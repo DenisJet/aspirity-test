bb6e1eb430bdf0950de4269b551b9f9cff3c0e4f
Denis Kalkopf <deniskalkopf@gmail.com>
2024-05-31 14:48:32 +0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const user = mockData[0];
 
 export default function Home() {
   return (
-    <main className='max-w-screen-2xl  mx-auto'>
+    <main className='max-w-screen-2xl mx-auto '>
       <a
         href=''
         title='назад'
@@ -14,17 +14,25 @@ export default function Home() {
         <Image src='chevron-left.svg' alt='назад' width={24} height={24} />
         Вернуться к сотрудникам
       </a>
-      <div className='py-8 px-4 bg-gray-800 rounded-xl'>
-        <Image className='m-auto pb-4' src={user.avatar} alt='аватар пользователя' width={160} height={160} />
-        <div>
-          <p className='font-bold text-2xl text-center mb-6'>
-            {user.lastName}
-            <br />
-            {user.firstName}
-          </p>
-          <div className='text-center font-normal text-sm mb-6'>
-            <p className='mb-4'>{user.position}</p>
-            <p>{user.address}</p>
+      <div className='py-8 px-4 bg-gray-800 md:px-10  md:pt-10 md:pb-5 rounded-xl'>
+        <div className='max-w-screen-2xl  mx-auto md:flex'>
+          <Image
+            className='m-auto pb-4 md:ms-0 md:me-12 md:mt-0'
+            src={user.avatar}
+            alt='аватар пользователя'
+            width={160}
+            height={160}
+          />
+          <div>
+            <p className='font-bold text-2xl text-center mb-6 md:text-5xl md:text-start md:leading-tight md:mb-8 md:mt-6'>
+              {user.lastName}
+              <br />
+              {user.firstName}
+            </p>
+            <div className='text-center font-normal text-sm mb-6 md:text-start md:mb-12'>
+              <p className='mb-4'>{user.position}</p>
+              <p>{user.address}</p>
+            </div>
           </div>
         </div>
       </div>
